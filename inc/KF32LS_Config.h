2cb4434112bef1e7056b969b87946236d8ffe559
ChipON-KungFu <ae@chipon-ic.com>
2021-01-17 18:31:28 +0800

    ●13 修改了整个GPIO_Output_LED ●12 system_init.c种SystemInit函数添加FLASH_CFG=0xC3配置,修改FLASH的读取周期为4周期 ●11 修改了整个GPIO_Output_LED ●10 更新IIC程序,更新IIC_DMA程序 ●9 更新FLASH程序,增加byte，word，doubleword的读写操作。删除老的单子（64bit）读写问题代码 ●8 为方便更新，标准库文件添加放到客户端处理 ●7 外部中断源的宏定义增加 ●6 INT_External_Source_Enable (uint32_t EintNum, uint32_tPeripheralSource)函数上的注释增加 ●5 修改TOUCH的位错误CT_CMPCFG_CMPPHASE_POS等16-27改为15-26 ●4 115200 z:8  x:1  y:15 ●3 GPIO_Pin_RMP_Config(GPIOF_SFR,GPIO_Pin_Num_1,GPIO_RMP_AF7_SPI1);//重映射为SPI3 ●2 增加#ifdef USE_CHECK_ASSERT ●1 增加#ifdef Perph_lcd_close

---
+++ inc/KF32LS_Config.h
@@ -0,0 +1,182 @@
+/**
+  *********************************************************************
+  * 文件名 KF32L_Config.h
+  * 作  者  ChipON_AE/FAE_Group
+  * 版  本  V2.2
+  * 日  期  2019-11-16
+  * 描  述  KF32L系列外设配置
+  *
+  *********************************************************************
+  */
+
+#ifndef _KF32LS_Config_H_
+#define _KF32LS_Config_H_
+
+/* 型号定义 */
+#ifdef KF32LS200FQS
+	#define KF32LS200
+#elif defined KF32LS200GQS
+    #define KF32LS200
+#elif defined KF32LS200IQS
+	#define KF32LS200
+#elif defined KF32LS200IQT
+	#define KF32LS200
+#elif defined KF32LS200KQT
+	#define KF32LS200
+#elif defined KF32LS200KQV
+	#define KF32LS200
+#elif defined KF32LS200MQV
+	#define KF32LS200
+#endif
+#if defined KF32LS200KQT
+	#define LS200_UASART_EX
+#elif defined KF32LS200KQV
+	#define LS200_UASART_EX
+#elif defined KF32LS200MQV
+	#define LS200_UASART_EX
+#endif
+
+#ifdef KF32LS500GQT
+	#define KF32LS500
+#elif defined KF32LS500IQT
+    #define KF32LS500
+#elif defined KF32LS500KQT
+	#define KF32LS500
+#elif defined KF32LS500MQT
+	#define KF32LS500
+#elif defined KF32LS500KQV
+	#define KF32LS500
+#elif defined KF32LS500MQV
+	#define KF32LS500
+#endif
+#if defined KF32LS500KQT
+	#define LS500_UASART_EX
+#elif defined KF32LS500MQT
+	#define LS500_UASART_EX
+#elif defined KF32LS500KQV
+	#define LS500_UASART_EX
+#elif defined KF32LS500MQV
+	#define LS500_UASART_EX
+#endif
+
+
+
+/* 外设资源 */
+#ifdef KF32LS200
+    /* ADC */
+	#define KF32LS_Periph_adc
+	#define KF32LS_Periph_adc0
+    /* CFGL */
+    #define KF32LS_Periph_cfgl
+	/* AES */
+	#define KF32LS_Periph_aes
+	/* CRC */
+	#define KF32LS_Periph_crc
+	/* DMA */
+	#define KF32LS_Periph_dma
+	#define KF32LS_Periph_dma0
+	#define KF32LS_Periph_dma1
+	/* I2C */
+    #define KF32LS_Periph_i2c
+    #define KF32LS_Periph_i2c0
+	#define KF32LS_Periph_i2c1
+	/* LCD */
+	#define KF32LS_Periph_lcd
+	/* QEI */
+	#define KF32LS_Periph_qei
+	#define KF32LS_Periph_qei0
+	/* SPI */
+	#define KF32LS_Periph_spi
+	#define KF32LS_Periph_spi0
+	#define KF32LS_Periph_spi1
+    /* USART */
+	#define KF32LS_Periph_usart
+	#define KF32LS_Periph_usart0
+	#define KF32LS_Periph_usart1
+	#define KF32LS_Periph_usart2
+	/* BTIME */
+	#define KF32LS_Periph_btime14
+	#define KF32LS_Periph_btime15
+	/* GPTIME */
+	#define KF32LS_Periph_gptime0
+	#define KF32LS_Periph_gptime1
+	#define KF32LS_Periph_gptime2
+	#define KF32LS_Periph_gptime3
+	#define KF32LS_Periph_gptime4
+	/* ATIME */
+	#define KF32LS_Periph_atime5_time6
+	#define KF32LS_Periph_atime5
+	#define KF32LS_Periph_atime6
+#endif //KF32LS200
+#ifdef LS200_UASART_EX
+	#define KF32LS_Periph_usart3
+	#define KF32LS_Periph_usart4
+	#define KF32LS_Periph_usart5
+	#define KF32LS_Periph_usart6
+	#define KF32LS_Periph_usart7
+#endif //LS200_UASART_EX
+
+
+#ifdef KF32LS500
+	/* ADC */
+	#define KF32LS_Periph_adc
+	#define KF32LS_Periph_adc0
+    /* CFGL */
+    #define KF32LS_Periph_cfgl
+	/* AES */
+	#define KF32LS_Periph_aes
+	/* CRC */
+	#define KF32LS_Periph_crc
+	/* CAN */
+	#define KF32LS_Periph_can
+	#define KF32LS_Periph_can1
+	/* CTOUCH */
+	#define KF32LS_Periph_ctouch
+	/* DMA */
+	#define KF32LS_Periph_dma
+	#define KF32LS_Periph_dma0
+	#define KF32LS_Periph_dma1
+	/* I2C */
+    #define KF32LS_Periph_i2c
+    #define KF32LS_Periph_i2c0
+	#define KF32LS_Periph_i2c1
+	/* LCD */
+	#define KF32LS_Periph_lcd
+	/* QEI */
+	#define KF32LS_Periph_qei
+	#define KF32LS_Periph_qei0
+	/* SPI */
+	#define KF32LS_Periph_spi
+	#define KF32LS_Periph_spi0
+	#define KF32LS_Periph_spi1
+    /* USART */
+	#define KF32LS_Periph_usart
+	#define KF32LS_Periph_usart0
+	#define KF32LS_Periph_usart1
+	#define KF32LS_Periph_usart2
+	/* BTIME */
+	#define KF32LS_Periph_btime14
+	#define KF32LS_Periph_btime15
+	/* GPTIME */
+	#define KF32LS_Periph_gptime0
+	#define KF32LS_Periph_gptime1
+	#define KF32LS_Periph_gptime2
+	#define KF32LS_Periph_gptime3
+	#define KF32LS_Periph_gptime4
+	/* ATIME */
+	#define KF32LS_Periph_atime5_time6
+	#define KF32LS_Periph_atime5
+	#define KF32LS_Periph_atime6
+    /* USB */
+	#define KF32LS_Periph_usb
+#endif
+#ifdef LS500_UASART_EX
+	#define KF32LS_Periph_usart3
+	#define KF32LS_Periph_usart4
+	#define KF32LS_Periph_usart5
+	#define KF32LS_Periph_usart6
+	#define KF32LS_Periph_usart7
+#endif //LS500_UASART_EX
+
+
+#endif /* _KF32L_Config_H */
